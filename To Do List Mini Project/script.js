6aa94f7973c196c2e93bedf6affbe9227e4dc77d
maria <mariyetavishanthi11@gmail.com>
2025-03-15 14:12:38 +0530

    js code

--- To Do List Mini Project/script.js
+++ To Do List Mini Project/script.js
@@ -1,6 +1,8 @@
+//added ids from html code here
 const inputBox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
  
+//add task function
 function addTask(){
     if(inputBox.value === ''){
         alert("Hey Write Something!");
@@ -9,6 +11,20 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    //to clear searchbar again
+    inputBox.value = "";
 
 }
+
+listcontainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
