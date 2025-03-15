5342dcbc4ed0281f21312c21fad0f0154c73ec14
maria <mariyetavishanthi11@gmail.com>
2025-03-15 13:36:25 +0530

    more changes

--- To Do List Mini Project/script.js
+++ To Do List Mini Project/script.js
@@ -2,5 +2,13 @@ const inputBox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
  
 function addTask(){
+    if(inputBox.value === ''){
+        alert("Hey Write Something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+    }
 
 }
